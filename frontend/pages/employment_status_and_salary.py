cee87bc5baa1439373526dbaeb903235440bd55c
sheik <sheiknoorullah@gmail.com>
2025-03-08 11:17:54 +0530

    added bar chart for employment status and salary

--- frontend/pages/employment_status_and_salary.py
+++ frontend/pages/employment_status_and_salary.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from components.data_fetcher import fetch_data
 import pandas as pd
+import plotly.express as px
 
 
 def main():
@@ -9,6 +10,8 @@ def main():
     if relationship:
         df = pd.DataFrame(relationship)
         st.dataframe(df)
+        fig = px.bar(df, x="Employment Status", y="Average Salary")
+        st.plotly_chart(fig)
 
 
 if __name__ == "__main__":
